6527d57c0bc3a1570d31da9c50e3608a6a7a6c82
Bittu <bittu@richestsoft.in>
2022-07-15 20:07:12 +0530

    map

--- lib/FS/MapSample/MapPage.dart
+++ lib/FS/MapSample/MapPage.dart
@@ -7,6 +7,11 @@ import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:intelligent_reader_app/Constants/ImagesString.dart';
 import 'package:intelligent_reader_app/Constants/app_strings.dart';
 import 'package:location/location.dart';
+import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
+
+import '../../Constants/AppFontFamily.dart';
+import '../../Constants/app_color.dart';
+import '../../Constants/app_widgetsize.dart';
 
 class MapPage extends StatefulWidget {
   @override
@@ -18,22 +23,33 @@ class _mapPageState extends State<MapPage> {
 
   var sizeVal;
   Location locationObj = Location();
-  LatLng currentLocation= LatLng(0.0, 0.0);
+  double curLat = 0.0;
+  double curLng = 0.0;
   Set<Marker> _markers = {};
-  bool _serviceEnabled=false;
+  bool _serviceEnabled = false;
   PermissionStatus? _permissionGranted;
+
   @override
   void initState() {
     checkPermissions();
+    try {
+      getCurrentLocation();
+    }
+    catch (exp) {
+      debugPrint("excep= ${exp}");
+    }
+
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    sizeVal = MediaQuery.of(context).size;
+    sizeVal = MediaQuery
+        .of(context)
+        .size;
     return Scaffold(
       body: Container(
-        height: sizeVal.height  ,
+        height: sizeVal.height,
         width: sizeVal.width,
         child: Stack(
           children: [
@@ -42,35 +58,44 @@ class _mapPageState extends State<MapPage> {
                 Container(
                   height: sizeVal.height * 0.8,
                   width: sizeVal.width,
-                  child: GoogleMap(
-                    zoomControlsEnabled: false,
-                    initialCameraPosition: CameraPosition(
-                      target: currentLocation == null
-                          ? LatLng(30.70, 76.71)
-                          : LatLng(currentLocation!.latitude!,
-                              currentLocation!.longitude!),
-                      zoom: 12.0,
-                    ),
-                    onMapCreated: (GoogleMapController controller) {
-                      _controller = controller;
-                    },
-                    onCameraMove: _onCameraMove,
-                    markers: _markers,
+                  child: Stack(
+                      children: [ GoogleMap(
+                        zoomControlsEnabled: false,
+                        initialCameraPosition: CameraPosition(
+                          target: curLat == 0.0
+                              ? const LatLng(30.70, 76.71)
+                              : LatLng(curLat,
+                              curLng),
+                          zoom: 12.0,
+                        ),
+                        onMapCreated: (GoogleMapController controller) {
+                          _controller = controller;
+                        },
+                        onCameraMove: _onCameraMove,
+                        markers: _markers,
+                      ),
+                        Center(
+                          child: Image.asset(ImagesString.homePng),
+                        )
+
+
+                      ]
+
                   ),
                 ),
-GestureDetector(
-  onTap: (){
-    debugPrint("sdfsdfsdf");
+                GestureDetector(
+                  onTap: () {
+                    showBottomSht(context);
+                  },
+                  child: Container(height: 40, width: 40, color: Colors.green,),
+                )
 
-  },
-  child: Container(height: 40,width: 40,color: Colors.green,),
-)
+
               ],
             ),
             Container(
-        color: Colors.pink,
               margin: EdgeInsets.only(
-                top: sizeVal.height * 0.1,
+                top: sizeVal.height * 0.7,
               ),
               height: sizeVal.height * 0.1,
               child: Align(
@@ -81,8 +106,8 @@ GestureDetector(
                     color: Colors.white,
                   ),
                   onPressed: () {
-                 //   getCurrentLocation();
-                  debugPrint("sdfsdfsdf");
+                    getCurrentLocation();
+                    //   debugPrint("sdfsdfsdf");
                   },
                 ),
               ),
@@ -94,21 +119,29 @@ GestureDetector(
   }
 
   void _onCameraMove(CameraPosition position) {
-    currentLocation = position.target ;
+    curLat = position.target.latitude;
+    curLng = position.target.longitude;
+    debugPrint(
+        "_onCameraMove   ${position.target.longitude}   ,  ${position.target
+            .longitude}");
   }
 
 
   void getCurrentLocation() async {
-    BitmapDescriptor?  customIcon;
-    BitmapDescriptor.fromAssetImage(ImageConfiguration(size: Size(12, 12)),
-        ImagesString.homePng)
-        .then((d) {
-      customIcon = d;
-    });
+    debugPrint("current loc fun calling");
 
- var   currentLoc = await locationObj.getLocation();
- currentLocation=LatLng(currentLoc.latitude!,currentLoc.longitude!);
-    locationObj.onLocationChanged.listen((LocationData loc) {
+    var currentLoc = await locationObj.getLocation();
+    debugPrint("current loc=${currentLoc.latitude}  ${currentLoc.longitude}");
+    curLat = currentLoc.latitude!;
+    curLng = currentLoc.longitude!;
+
+    _controller
+        ?.animateCamera(CameraUpdate.newCameraPosition(new CameraPosition(
+      target: LatLng(curLat, curLng),
+      zoom: 12.0,
+    )));
+
+    /*locationObj.onLocationChanged.listen((LocationData loc) {
       _controller
           ?.animateCamera(CameraUpdate.newCameraPosition(new CameraPosition(
         target: LatLng(loc.latitude ?? 0.0, loc.longitude ?? 0.0),
@@ -117,13 +150,14 @@ GestureDetector(
       print("loc.latitude  ${loc.latitude}");
       print("loc.longitude  ${loc.longitude}");
       setState(() {
-        _markers.add(Marker(
-            markerId: MarkerId('Home'),
-            position: LatLng(loc.latitude ?? 0.0, loc.longitude ?? 0.0),
-            icon: customIcon!,
-          infoWindow: InfoWindow(title: currentLocation.toString(), snippet: "go here"),));
-      });
+      _markers.add(Marker(
+        markerId: MarkerId('Home'),
+        position: LatLng(curLat, curLng),
+        icon: customIcon!,
+        infoWindow: InfoWindow(title: ('$curLat, $curLng').toString(), snippet: "go here"),));
     });
+    });*/
+
   }
 
   Future<void> checkPermissions() async {
@@ -144,6 +178,90 @@ GestureDetector(
     }
 
     getCurrentLocation();
-
   }
+
+    void showBottomSht(BuildContext context) {
+      showModalBottomSheet(
+          context: context,
+          builder: (BuildContext bc){
+            return Container(
+              margin: EdgeInsets.only(left: 10,right: 10),
+
+              child: Wrap(
+                children: <Widget>[
+                  Row(
+mainAxisAlignment: MainAxisAlignment.spaceBetween,                    children: [
+
+                    SizedBox(),
+                    Text("Add New Address"),
+                    IconButton(onPressed: (){
+                    }, icon: Image.asset(ImagesString.circle_grey_filled))
+                  ],),
+                  emailField(),
+                  emailField(),
+                  emailField(),
+                  emailField(),
+                  submitButton()
+                ],
+              ),
+            );});
+     }
+
+
+  Widget emailField() {
+    return Container(
+      margin: EdgeInsets.only(top: MediaQuery.of(context).size.height * 0.01),
+      child:  TextFormField(
+       // controller: emailController,
+        style: TextStyle(color: Colors.black,fontFamily: AppFontFamily.RobotoMedium),
+        maxLines: 1,
+        decoration:  InputDecoration(
+          enabledBorder: UnderlineInputBorder(borderSide: BorderSide(color: AppColor.appBlackColor)),
+          focusedBorder: UnderlineInputBorder(borderSide: BorderSide(color: AppColor.appOrangeColor)),
+          border: UnderlineInputBorder(),
+          fillColor: AppColor.appHintColor,
+          labelText: 'Email Address',
+          labelStyle: TextStyle(
+              fontSize: MediaQuery.of(context).size.height * AppWidgetSize.appContentSmallFontSize,
+              color: AppColor.appHintColor,
+              fontFamily: AppFontFamily.RobotoMedium),
+        ),
+      ),
+    );
+  }
+
+  Widget submitButton() {
+    return GestureDetector(
+      onTap: ()
+      {
+        Navigator.pop(context);
+    //    updateProfileApi();
+
+      },
+      child: Center(
+        child: Container(
+          alignment: Alignment.center,
+          margin: EdgeInsets.only(top: MediaQuery.of(context).size.height * AppWidgetSize.sideMargins),
+          width: MediaQuery.of(context).size.width * AppWidgetSize.appButtonWidth,
+          height: MediaQuery.of(context).size.height * AppWidgetSize.appButtonHeight,
+          decoration: BoxDecoration(
+
+            color: AppColor.appOrangeColor,
+            borderRadius: BorderRadius.all(
+                Radius.circular(MediaQuery.of(context).size.width * AppWidgetSize.appButtonBorderRadius)),
+          ),
+          child:  Text(
+              "Submit",
+              style: TextStyle(color: AppColor.appWhiteColor,
+                  fontSize: MediaQuery.of(context).size.height * AppWidgetSize.appContentFontSize,
+                  fontFamily: AppFontFamily.RobotoMedium
+              )),
+        ),
+      ),
+    );
+  }
+
+
 }
+
+
